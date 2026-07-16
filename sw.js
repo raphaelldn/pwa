e3dc6ea4e081d4a4a73eebaefa62d77a22ab5fd6
raphaelldn <raphael18@gmail.com>
2026-07-15 23:34:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cafes-proto-v2';
+const CACHE = 'cafes-proto-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
